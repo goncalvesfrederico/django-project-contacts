3ce6e9c500744f0df8665b7d899ddf4aae800b6e
goncalvesfrederico <fredericoaraujogoncalves@gmail.com>
2024-02-03 14:50:26 -0300

    I manipulated the QuerySets Django with filter and order by.

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by("-id")
+
+    # I'm seeing what my SQL QUERY is doing in backend
+    print(contacts.query)
 
     context = {
         "contacts": contacts
