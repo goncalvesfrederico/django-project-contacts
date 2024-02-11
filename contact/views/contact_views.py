11e97397e6b691b4c16ec9712cf68152389cfedc
goncalvesfrederico <fredericoaraujogoncalves@gmail.com>
2024-02-11 17:14:02 -0300

    I created the url, view and template of search. I made the QuerySet worked using the field lookups and class Q (import from django.db.models)

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
-from django.http import Http404
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by("-id")
@@ -29,5 +29,31 @@ def contact(request, contact_id):
     return render(
         request,
         'contact/contact.html',
+        context
+    )
+
+
+def search(request):
+    search_value = request.GET.get("q", "").strip()
+
+    if search_value == "":
+        return redirect("contact:index")
+
+    contacts = Contact.objects.filter(show=True).filter(
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value)
+    ).order_by("-id")
+    print(contacts.query)
+
+    context = {
+        "contacts": contacts,
+        "site_title": "Search -"
+    }
+
+    return render(
+        request,
+        "contact/index.html",
         context
     )
