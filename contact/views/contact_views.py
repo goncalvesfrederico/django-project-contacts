f64c8914e04213371cc37dc7426f9fd415a4b989
goncalvesfrederico <fredericoaraujogoncalves@gmail.com>
2024-02-11 17:38:20 -0300

    Included the search_value in context to show the value searched. I made if statement in index.html (If contacts is True show the contacts else showed the message).

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -49,7 +49,8 @@ def search(request):
 
     context = {
         "contacts": contacts,
-        "site_title": "Search -"
+        "site_title": "Search -",
+        "search_value": search_value,
     }
 
     return render(
