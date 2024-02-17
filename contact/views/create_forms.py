ba7dbedc896e64b3d88fbcfcd1658d25edee7e65
goncalvesfrederico <fredericoaraujogoncalves@gmail.com>
2024-02-17 10:56:01 -0300

    I created a form file and I created a ContactForm class to get the first_name, last_name and phone. Then I passed the fields to my craete.html to handle and show the forms fields. I inputed the non_fields_errors in my create.html to showing the errors it they exist.

--- contact/views/create_forms.py
+++ contact/views/create_forms.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.db.models import Q
-from django.core.paginator import Paginator
-from contact.models import Contact
+from contact.forms import ContactForm
 
 def create(request):
-    contacts = Contact.objects.filter(show=True).order_by("-id")
+    if request.method == "POST":
+        context = {
+            "form": ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            "contact/create.html",
+            context,
+        )
 
     context = {
-        
+        "form": ContactForm()
     }
 
     return render(
