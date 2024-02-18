23755fd1a00f1c9f7898dc17b9b4374e498c7ae9
goncalvesfrederico <fredericoaraujogoncalves@gmail.com>
2024-02-18 16:22:17 -0300

    I created the URL and VIEW of UPDATE CRUD. I changed the form action of my create.html template, because I want to get the URL dynamically.

--- contact/forms.py
+++ contact/forms.py
@@ -17,7 +17,7 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = (
-            "first_name", "last_name", "phone",
+            "first_name", "last_name", "phone", "email", "description", "category",
         )
         
     def clean(self):
