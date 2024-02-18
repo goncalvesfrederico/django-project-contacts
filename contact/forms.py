ed8afd4da42226ee2dfe48af71a207e84a332087
goncalvesfrederico <fredericoaraujogoncalves@gmail.com>
2024-02-18 14:16:06 -0300

    I created the clean method to have been used to validation the form fields (first_name and last_name)

--- contact/forms.py
+++ contact/forms.py
@@ -1,9 +1,30 @@
 from django import forms
 from contact.models import Contact
+from django.core.exceptions import ValidationError
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "class-a class-b",
+                "placeholder": "First Name",
+            }
+        ),
+        label="First Name",
+        help_text="Input your first name",
+    )
+
     class Meta:
         model = Contact
         fields = (
             "first_name", "last_name", "phone",
         )
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        first_name = cleaned_data.get("first_name")
+        last_name = cleaned_data.get("last_name")
+
+        if first_name == last_name:
+            msg = "The First Name and Last Name could not be the same!!!"
+            self.add_error("last_name", ValidationError(msg))
